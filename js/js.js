7bb2e7a0d97eed71867b1e127ef5139a02dc5299
tomasbrown1 <tombrown1@gmail.com>
2015-04-06 11:26:17 +0100

    fixed nav menu, added sample text and started making site responsive

--- js/js.js
+++ js/js.js
@@ -1,19 +1,28 @@
 $(function() {
 	var $red = $('#redbox');
 	var $green = $('#greenbox');
-	var $blue = $('#bluebox')
+	var $blue = $('#bluebox');
 	
 	$red.on('click', function() {
 			$('header, footer').removeClass("backgroundred backgroundgreen backgroundblue").addClass("backgroundred");
-	})
+	});
 	
 	$green.on('click', function() {
 			$('header, footer').removeClass("backgroundred backgroundgreen backgroundblue").addClass("backgroundgreen");
-	})
+	});
 	
 	$blue.on('click', function() {
 			$('header, footer').removeClass("backgroundred backgroundgreen backgroundblue").addClass("backgroundblue");
-	})
+	});
 
+	
+	var $menu = $('.menu');
+	var $button = $('.button');
+
+	$menu.hide()
+	
+	$button.on('click', function() {
+		$menu.slideToggle(300);
+	});
 	
 });
